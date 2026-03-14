6e9834897b10fb90e0213a8acd94a3b77a3eaaf9
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-03-15 00:46:55 +0100

    set up the files and folders and makse some basic design for the quizz page

---
+++ src/app/quizz/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+const questions = [
+  {
+    questionText: "What is your level of understanding React?",
+    answers: [
+      { answerText: "Beginner", isCorrect: true, id: 1 },
+      { answerText: "Intermediate", isCorrect: false, id: 2 },
+      { answerText: "Advanced", isCorrect: false, id: 3 },
+      { answerText: "Expert", isCorrect: false, id: 4 },
+    ],
+  },
+  {
+    questionText: "What is the virtual DOM in React?",
+    answers: [
+      { answerText: "A representation of the real DOM", isCorrect: true, id: 1 },
+      { answerText: "A new JavaScript framework", isCorrect: false, id: 2 },
+      { answerText: "A type of database", isCorrect: false, id: 3 },
+      { answerText: "A CSS library", isCorrect: false, id: 4 },
+    ],
+  },
+  {
+    questionText: "What is JSX in React?",
+    answers: [
+      { answerText: "A syntax extension for JavaScript", isCorrect: true, id: 1 },  
+      { answerText: "A new programming language", isCorrect: false, id: 2 },
+      { answerText: "A type of API", isCorrect: false, id: 3 },
+      { answerText: "A CSS preprocessor", isCorrect: false, id: 4 },
+    ],
+  }
+];
+
+export default function Home() {
+  const [started, setStarted] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleNext = () => {
+    setStarted(true);
+  };
+
+  return (
+    <div className="flex flex-col flex-1">
+    <main className="flex justify-center flex-1">
+      {!started ?
+       <h1 className="text-3xl font-bold">Welcome to the quizz page 👋</h1> 
+      :
+       <div>
+        <h2 className="text-3xl font-bold">
+          What is your level of understanding React
+        </h2>
+        <div className="grid grid-cols-1 gap-6 mt-6">
+          <Button variant={"secondary"}>Beginner</Button>
+          <Button variant={"secondary"}>Intermediate</Button>
+          <Button variant={"secondary"}>Advanced</Button>
+          <Button variant={"secondary"}>Expert</Button>
+        </div>
+      </div>}
+    </main>
+    <footer className="footer pb-9 px-6 relative mb-0 ">
+      <Button onClick={handleNext}>{!started ? 'Start' : 'Next'}</Button>
+    </footer>
+    </div>
+  )
+}
