8b523280bcfab6dd3f51c952c7cc4a57cace39bf
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-03-15 05:09:09 +0100

    adding Quiz UI Components + design the options buttons and the result page

--- src/app/quizz/page.tsx
+++ src/app/quizz/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import ProgressBar from "@/components/ui/progressBar";
+import { ChevronLeft, X } from "lucide-react";
+import ResultCard from "./ResultCard";
+import { set } from "react-hook-form";
+import QuizzSubmission from "./QuizzSubmission";
 
 const questions = [
   {
@@ -31,23 +36,66 @@ const questions = [
     ],
   }
 ];
-
 export default function Home() {
-  const [started, setStarted] = useState(false);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [started, setStarted] = useState<boolean>(false);
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  const [isCorrect, setCorrect] = useState<boolean | null>(null);
+  const [submitted, setSubmitted] = useState<boolean>(false);
 
   const handleNext = () => {
     if(!started) {
     setStarted(true);
+    return;
     }
-
     if(currentQuestion < questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
+    }else {
+      setSubmitted(true);
+      return;
     }
+    setSelectedAnswer(null);
+    setCorrect(null);
   };
+
+  const scorePercentage: number = Math.round((score / questions.length) * 100);
+
+  const handleAnswer = (answer) => {
+    setSelectedAnswer(answer.id);
+    const isCurrentCorrect = answer.isCorrect;
+    if(isCurrentCorrect) {
+      setScore(score + 1);
+    }
+    setCorrect(isCurrentCorrect);
+  }
+
+    if(submitted) {
+      return (
+        <QuizzSubmission score={score}
+         totalQuestions={questions.length}
+        scorePercentage={scorePercentage} />
+      )
+    }
 
   return (
     <div className="flex flex-col flex-1">
+      <div className="position-sticky top-0 z-10 shadow-md py-4 w-full">
+        <header className="grid grid-cols-[auto,1fr,auto]
+          grid-flow-col items-center justify-between py-2 gap-2">
+
+          <Button size="icon"
+           variant="outline"><ChevronLeft />
+           </Button>
+
+          <ProgressBar value={(currentQuestion / questions.length) * 100} />
+           
+           <Button size="icon"
+           variant="outline"><X />
+           </Button>
+          
+        </header>
+      </div>
     <main className="flex justify-center flex-1">
       {!started ?
        <h1 className="text-3xl font-bold">Welcome to the quizz page 👋</h1> 
@@ -59,9 +107,13 @@ export default function Home() {
         <div className="grid grid-cols-1 gap-6 mt-6">
           {
             questions[currentQuestion].answers.map(answer => {
+              const variant = selectedAnswer === answer.id ? (answer.isCorrect ? "neoSuccess" : "neoDanger") : "neoOutline";
+              
               return (
-                <Button key={answer.id} variant={"secondary"} >
-                  {answer.answerText}
+                <Button key={answer.id} variant={variant}
+                  size="xl"
+                 onClick={() => handleAnswer(answer)}>
+                  <p className="whitespace-normal">{answer.answerText}</p>  
                 </Button>
               )
             })
@@ -70,7 +122,9 @@ export default function Home() {
       </div>}
     </main>
     <footer className="footer pb-9 px-6 relative mb-0 ">
-      <Button onClick={handleNext}>{!started ? 'Start' : 'Next'}</Button>
+      <ResultCard isCorrect={isCorrect}
+       correctAnswer={questions[currentQuestion].answers.find(answer => answer.isCorrect === true)?.answerText} />
+      <Button variant="neo" size="lg" onClick={handleNext}>{!started ? 'Start' : (currentQuestion === questions.length -1) ? 'Submit' : 'Next'}</Button>
     </footer>
     </div>
   )
