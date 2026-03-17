ff9a770d9c57daaebfe26dd9b6d8cb05733093f3
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-03-17 03:33:06 +0100

    Fetching Data + Refactor QuizzQuestions and ResultCard components; add user answer tracking and navigation features

--- src/app/quizz/QuizzQuestions.tsx
+++ src/app/quizz/QuizzQuestions.tsx
@@ -4,10 +4,10 @@ import { Button } from "@/components/ui/button";
 import ProgressBar from "@/components/ui/progressBar";
 import { ChevronLeft, X } from "lucide-react";
 import ResultCard from "./ResultCard";
-import { set } from "react-hook-form";
 import QuizzSubmission from "./QuizzSubmission";
 import { InferSelectModel } from "drizzle-orm";
 import { questionAnswers, questions as Dbquestions, quizzes } from "@/db/schema";
+import { useRouter } from "next/navigation";
 
 type Answer = InferSelectModel<typeof questionAnswers>;
 type Question = InferSelectModel<typeof Dbquestions> & {
@@ -22,49 +22,69 @@ type props = {
 }
 
 
-
 export default function QuizzQuestions(props: props) {
   const { questions } = props.quizz;
   const [started, setStarted] = useState<boolean>(false);
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   const [score, setScore] = useState<number>(0);
-  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
-  const [isCorrect, setCorrect] = useState<boolean | null>(null);
+  const [userAnswers, setUserAnswers] = useState<{ questionId: number, answerId: number }[]>([]);
   const [submitted, setSubmitted] = useState<boolean>(false);
+  const router = useRouter();
 
   const handleNext = () => {
-    if(!started) {
-    setStarted(true);
-    return;
+    if (!started) {
+      setStarted(true);
+      return;
     }
-    if(currentQuestion < questions.length - 1) {
+    if (currentQuestion < questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
-    }else {
+    } else {
       setSubmitted(true);
       return;
     }
-    setSelectedAnswer(null);
-    setCorrect(null);
   };
 
-  const scorePercentage: number = Math.round((score / questions.length) * 100);
+  const handleAnswer = (answer: Answer, questionId: number) => {
+    const newUserAnswersArr = [...userAnswers, {
+      questionId,
+      answerId: answer.id,
+    }];
+    setUserAnswers(newUserAnswersArr);
 
-  const handleAnswer = (answer: Answer) => {
-    setSelectedAnswer(answer.id);
     const isCurrentCorrect = answer.isCorrect;
-    if(isCurrentCorrect) {
+    if (isCurrentCorrect) {
       setScore(score + 1);
     }
-    setCorrect(isCurrentCorrect);
   }
 
-    if(submitted) {
-      return (
-        <QuizzSubmission score={score}
-         totalQuestions={questions.length}
-        scorePercentage={scorePercentage} />
-      )
+  const handlePressPrev = () => {
+    if (currentQuestion !== 0) {
+      setCurrentQuestion(prevCurrentQuestion => prevCurrentQuestion - 1);
     }
+  }
+
+  const handleExit = () => {
+    router.push('/dashboard');
+  }
+
+  const scorePercentage: number = Math.round((score / questions.length) * 100);
+  const selectedAnswer: number | null | undefined = userAnswers.find((item) => item.questionId === questions[currentQuestion].id)?.answerId;
+  const isCorrect =
+    questions[currentQuestion].answers.findIndex(
+      (answer) => answer.id === selectedAnswer
+    ) !== -1
+      ? questions[currentQuestion].answers.find(
+        (answer) => answer.id === selectedAnswer
+      )?.isCorrect
+      : null;
+
+  if (submitted) {
+    return (
+      <QuizzSubmission score={score}
+        totalQuestions={questions.length}
+        scorePercentage={scorePercentage} />
+    )
+  }
 
   return (
     <div className="flex flex-col flex-1">
@@ -73,47 +93,50 @@ export default function QuizzQuestions(props: props) {
           grid-flow-col items-center justify-between py-2 gap-2">
 
           <Button size="icon"
-           variant="outline"><ChevronLeft />
-           </Button>
+            variant="outline"
+            onClick={handlePressPrev}><ChevronLeft />
+          </Button>
 
           <ProgressBar value={(currentQuestion / questions.length) * 100} />
-           
-           <Button size="icon"
-           variant="outline"><X />
-           </Button>
-          
+
+          <Button size="icon"
+            variant="outline"
+            onClick={handleExit}><X />
+          </Button>
+
         </header>
       </div>
-    <main className="flex justify-center flex-1">
-      {!started ?
-       <h1 className="text-3xl font-bold">Welcome to the quizz page 👋</h1> 
-      :
-       <div>
-        <h2 className="text-3xl font-bold">
-          {questions[currentQuestion].questionText}
-        </h2>
-        <div className="grid grid-cols-1 gap-6 mt-6">
-          {
-            questions[currentQuestion].answers.map(answer => {
-              const variant = selectedAnswer === answer.id ? (answer.isCorrect ? "neoSuccess" : "neoDanger") : "neoOutline";
-              
-              return (
-                <Button key={answer.id} variant={variant}
-                  size="xl"
-                 onClick={() => handleAnswer(answer)}>
-                  <p className="whitespace-normal">{answer.answerText}</p>  
-                </Button>
-              )
-            })
-          }
-        </div>
-      </div>}
-    </main>
-    <footer className="footer pb-9 px-6 relative mb-0 ">
-      <ResultCard isCorrect={isCorrect}
-       correctAnswer={questions[currentQuestion].answers.find(answer => answer.isCorrect === true)?.answerText || ""} />
-      <Button variant="neo" size="lg" onClick={handleNext}>{!started ? 'Start' : (currentQuestion === questions.length -1) ? 'Submit' : 'Next'}</Button>
-    </footer>
+      <main className="flex justify-center flex-1">
+        {!started ?
+          <h1 className="text-3xl font-bold">Welcome to the quizz page 👋</h1>
+          :
+          <div>
+            <h2 className="text-3xl font-bold">
+              {questions[currentQuestion].questionText}
+            </h2>
+            <div className="grid grid-cols-1 gap-6 mt-6">
+              {
+                questions[currentQuestion].answers.map(answer => {
+                  const variant = selectedAnswer === answer.id ? (answer.isCorrect ? "neoSuccess" : "neoDanger") : "neoOutline";
+
+                  return (
+                    <Button key={answer.id} disabled={!!selectedAnswer}
+                      variant={variant}
+                      size="xl"
+                      onClick={() => handleAnswer(answer, questions[currentQuestion].id)} className="disabled:opacity-100">
+                      <p className="whitespace-normal">{answer.answerText}</p>
+                    </Button>
+                  )
+                })
+              }
+            </div>
+          </div>}
+      </main>
+      <footer className="footer pb-9 px-6 relative mb-0 ">
+        <ResultCard isCorrect={isCorrect}
+          correctAnswer={questions[currentQuestion].answers.find(answer => answer.isCorrect === true)?.answerText || ""} />
+        <Button variant="neo" size="lg" onClick={handleNext}>{!started ? 'Start' : (currentQuestion === questions.length - 1) ? 'Submit' : 'Next'}</Button>
+      </footer>
     </div>
   )
 }
--- src/app/quizz/UploadDoc.tsx
+++ src/app/quizz/UploadDoc.tsx
@@ -2,11 +2,13 @@
 import { useState } from "react";
 import { set } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 const UploadDoc = () => {
   const [document, setDocument] = useState<Blob | File | null | undefined>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -23,7 +25,10 @@ const UploadDoc = () => {
         body: formData,
       });
       if (res.status === 200) {
-        console.log("quizz generated successfully");
+        const data = await res.json();
+        const quizzId = data.quizzId;
+
+        router.push(`/quizz/${quizzId}`);
     }
   } catch (e) {
       console.log("error generating quizz", e);
@@ -35,7 +40,7 @@ const UploadDoc = () => {
 
   return (
     <div className="w-full">
-      <form className="w-full" onSubmit={handleSubmit}>
+      {isLoading ? <p>Loading...</p> : <form className="w-full" onSubmit={handleSubmit}>
         <label htmlFor="document" className="bg-secondary w-full flex h-20 rounded-md border-4 border-dashed border-blue-900 relative">
 
           <div className="absolute inset-0 m-auto flex justify-center items-center">
@@ -51,7 +56,7 @@ const UploadDoc = () => {
         </Button>
 
 
-      </form>
+      </form>}
     </div>
 
   );
