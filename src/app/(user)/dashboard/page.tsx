12a7402d8635453ec5f52de6f0bf570d1a469633
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-03-19 04:46:18 +0100

    Aggregate functions and join tables

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { eq } from 'drizzle-orm';
 import { quizzes } from '@/db/schema';
 import { auth } from '@/auth';
 import QuizzesTable, { Quizz } from './quizzesTable';
-
+import getUserMatrics from '@/actions/getUserMatrics';
 
 
 
@@ -12,19 +12,18 @@ const page = async () => {
   const userId = session?.user?.id;
 
   if (!userId) {
-    return(<p>User not found</p>)
-  } 
+    return (<p>User not found</p>)
+  }
 
   // Fetch quizzes created by the user
   const userQuizzes: Quizz[] = await db.query.quizzes.findMany({
     where: eq(quizzes.userId, userId)
   });
-
-  console.log('User Quizzes:', userQuizzes);
-
+  const userData = await getUserMatrics();
+  console.log(userData);
 
   return (
-   <QuizzesTable quizzes={userQuizzes} />
+    <QuizzesTable quizzes={userQuizzes} />
   );
 }
 
---
+++ src/app/actions/getUserMatrics.ts
@@ -0,0 +1,55 @@
+import { quizzes, questions, questionAnswers, users, quizzSubmissions } from "@/db/schema";
+import { auth } from "@/auth";
+import { count, eq, avg } from "drizzle-orm";
+import { db } from "@/db";
+
+const getUserMatrics = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
+  if (!userId) {
+    return;
+  }
+
+  //get the total number of questions in the quizz
+  const numQuizzes = await db
+  .select({ value: count() })
+  .from(quizzes)
+  .where(eq(quizzes.userId, userId));
+
+
+  // get the total number of questions in the quizz
+  const numQuestions = await db
+  .select({value: count()})
+  .from(questions)
+  .innerJoin(quizzes, eq(questions.quizzId, quizzes.id))
+  .innerJoin(users, eq(quizzes.userId, users.id))
+  .where(eq(quizzes.userId, userId));
+
+
+    // get the total number of submissions for the quizz
+
+    const numSubmissions = await db
+    .select({value: count()})
+    .from(quizzSubmissions)
+    .innerJoin(quizzes, eq(quizzSubmissions.quizzId, quizzes.id))
+    .innerJoin(users, eq(quizzes.userId, users.id))
+    .where(eq(quizzes.userId, userId));
+
+    // get the average score for the quizz
+    const avgScore = await db
+    .select({ value: avg(quizzSubmissions.score) })
+    .from(quizzSubmissions)
+    .innerJoin(quizzes, eq(quizzSubmissions.quizzId, quizzes.id))
+    .innerJoin(users, eq(quizzes.userId, users.id))
+    .where(eq(quizzes.userId, userId));
+
+
+      return {
+         numQuizzes: numQuizzes[0], numQuestions: numQuestions[0],
+          numSubmissions: numSubmissions[0],
+          avgScore: avgScore[0],
+         };
+
+}
+
+export default getUserMatrics;
